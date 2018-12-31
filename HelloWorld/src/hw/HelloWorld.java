62ab9e2c539073d5adf69eb278bf9dbe57a5ea98
Jonathan Wang <jdwh08@gmail.com>
2018-12-31 13:56:28 -0500

    Changed this to be less like "Hello World" and more like "Country Roads".

--- HelloWorld/src/hw/HelloWorld.java
+++ HelloWorld/src/hw/HelloWorld.java
@@ -1,7 +1,28 @@
 package hw;
 
+import java.util.concurrent.TimeUnit;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		String[] westVirginia = {"Almost heaven, ", "West Virginia,", 
+				"Blue Ridge Mountain, ", "Shenandoah River",
+				"Life is old there, ", "older than the trees,",
+				"Younger than the mountains,", "blowing like a breeze",
+				"Country Roads, ", "take me home",
+				"To the place I belong,",
+				"West Virginia,",
+				"Mountain Momma, ", "take me home",
+				"Country roads"};
+		
+		for(int i = 0; i < westVirginia.length; i++) {
+			System.out.println(westVirginia[i]);
+			
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
